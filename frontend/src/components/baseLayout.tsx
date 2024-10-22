9504bfa7a5d048f55e4a6d522146c7fa24c9b9bf
Sparsh Modi <sparshmodi2016@gmail.com>
2024-10-21 21:21:31 -0400

    added 404 page

--- frontend/src/components/baseLayout.tsx
+++ frontend/src/components/baseLayout.tsx
@@ -2,15 +2,18 @@ import React, { ReactNode } from "react"
 import Header from "./header"
 import Footer from "./footer"
 import { Container } from "@mui/material"
+import { useRouter } from "next/router"
 
 const BaseLayout: React.FC<{children: ReactNode }> = ({children}) => {
+    const router = useRouter()
+    
     return (
     <Container
         disableGutters
         maxWidth={false}
         className="flex flex-col min-h-screen w-full mx-0"
     >
-        <Header />
+        {router.pathname !== '/404' && <Header />}
         <Container 
             disableGutters
             maxWidth={false} 
---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,25 @@
+import { goBackToHome, notFoundDescription, notFoundTitle } from "@/constants"
+import { Button, Container, Typography } from "@mui/material"
+import Link from "next/link"
+
+export const PageNotFound = () => {
+    return (
+    <Container 
+        disableGutters
+        maxWidth={false}
+        className="h-full absolute flex flex-col justify-center items-center p-10"
+        sx={{
+            backgroundImage: "url('mountain.webp')",
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }}
+    >
+        <Typography variant="h1">{notFoundTitle}</Typography>
+        <Typography variant="h6">{notFoundDescription}</Typography>
+        <Link href="/" className="mt-8">
+            <Button variant="contained" className="bg-blue-500">{goBackToHome}</Button>
+        </Link>
+        </Container>)
+}
+
+export default PageNotFound
