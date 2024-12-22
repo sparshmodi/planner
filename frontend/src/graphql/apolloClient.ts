51ac8c80a396468c79de936d14cd31823a2db196
Sparsh Modi <sparshmodi2016@gmail.com>
2024-12-22 17:40:00 -0500

    update eslint rule

--- frontend/src/graphql/apolloClient.ts
+++ frontend/src/graphql/apolloClient.ts
@@ -1,13 +1,15 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 import { DJANGO_BACKEND_URL, GRAPHQL_EP } from '../constants'
 
-export const createApolloClient = () => { return  new ApolloClient({
-	link: new HttpLink({
-		uri: DJANGO_BACKEND_URL + GRAPHQL_EP,
-		credentials: 'same-origin',
-	}),
-	cache: new InMemoryCache(),
-}) }
+export const createApolloClient = () => {
+	return  new ApolloClient({
+		link: new HttpLink({
+			uri: DJANGO_BACKEND_URL + GRAPHQL_EP,
+			credentials: 'same-origin',
+		}),
+		cache: new InMemoryCache(),
+	}) 
+}
 
 const client = createApolloClient()
 
