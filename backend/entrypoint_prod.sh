cea2895d2323b22102e354a592181c53c6415c70
Sparsh Modi <sparshmodi2016@gmail.com>
2025-01-02 22:57:10 -0500

    remove cron related settings

--- backend/entrypoint_prod.sh
+++ backend/entrypoint_prod.sh
@@ -3,19 +3,6 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Start the cron service in the background
-echo "Starting cron service..."
-cron
-
-# Log cron output
-echo "Tailing the cron log..."
-touch /var/log/cron.log
-tail -f /var/log/cron.log &
-
-# Make migrations for the 'scheduler' app
-echo "Making migrations for scheduler..."
-python3 manage.py makemigrations scheduler
-
 # Apply all migrations
 echo "Applying migrations..."
 python3 manage.py migrate
