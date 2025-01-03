afa65221e540ec3d9934ce86e869831b41066ba9
Sparsh Modi <sparshmodi2016@gmail.com>
2025-01-02 20:14:16 -0500

    cache static assets

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,29 @@ const nextConfig = {
     output: 'standalone',
     images: {
         domains: ['lh3.googleusercontent.com'],
-    }
+    },
+    async headers() {
+    	return [
+    		{
+				source: '/icon.png',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 'public, max-age=31536000, immutable',
+					},
+				],
+			},
+			{
+				source: '/mountain.webp',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 'public, max-age=31536000, immutable',
+					},
+				],
+			},
+		];
+	},
 }
 
 module.exports = nextConfig
